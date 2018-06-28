7e040147cb8feb10b5a338f20fc9d3cacb4f5221
Shubham Agarwal <shubhamagarwal3010@gmail.com>
2018-06-28 15:38:05 +0530

    Added permissions validation for camera

--- app/src/main/java/com/vuforia/samples/VideoPlayback/ui/ActivityList/MainActivity.java
+++ app/src/main/java/com/vuforia/samples/VideoPlayback/ui/ActivityList/MainActivity.java
@@ -1,7 +1,11 @@
 package com.vuforia.samples.VideoPlayback.ui.ActivityList;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
@@ -28,8 +32,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void viewARVideo(View view) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
         Intent intent = new Intent(MainActivity.this, VideoPlayback.class);
         startActivity(intent);
+
     }
 
     public void viewARText(View view) {
